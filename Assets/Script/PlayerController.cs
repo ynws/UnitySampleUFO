87bd7d0d5b653ed8c1d1d8c5bc5a4b18d8e41766
ynws <ynw_soft@yahoo.co.jp>
2017-08-05 00:00:19 +0900

    03 add move script(NOTE: utf8, LF)

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+
+    private Rigidbody2D rb2d;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // frameごとの状態更新。ただし、物理演算の直前に呼ばれるので動きの制御に使うこと。
+    // ロジック的な舞frame処理はUpdate()で
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        rb2d.AddForce(movement * speed);
+    }
+}
