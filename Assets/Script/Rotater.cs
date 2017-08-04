794d29cae671b4b58681ef2afd7e508f7375cde2
ynws <ynw_soft@yahoo.co.jp>
2017-08-05 08:36:57 +0900

    06 add pickups

---
+++ Assets/Script/Rotater.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotater : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+        // Updateが呼び出されるのはframeごと。
+        // 処理落ち等で更新タイミングが一定にならないことがあるので、
+        // Time.deltaTimeを使って実時間に合わせた動きにする。
+        transform.Rotate(new Vector3(0, 0, 45) * Time.deltaTime);
+	}
+}
