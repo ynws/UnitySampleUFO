afdd154ddefc2f4a871fb57522584ee5e8761d08
ynws <ynw_soft@yahoo.co.jp>
2017-08-05 09:24:21 +0900

    08 add win text

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -1,16 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
 
     public float speed;
+    public Text countText;
+    public Text winText;
 
     private Rigidbody2D rb2d;
+
+    // 収集アイテム数。PlayerControllerが持ってるのは変じゃないか？
+    // まぁ、チュートリアルだから簡略化されてるだけだろうけど。
+    private int count;
 
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
     }
 
     // frameごとの状態更新。ただし、物理演算の直前に呼ばれるので動きの制御に使うこと。
@@ -29,7 +39,21 @@ public class PlayerController : MonoBehaviour {
         if (other.CompareTag("PickUp"))
         {
             other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
         }
+    }
 
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+
+        // pickupの合計数をPlayerが知っているのは変。
+        // countの保持とともに、ステージ管理用objでも作ってそっちに投げるのが自然。
+        // まぁ、チュートリアルなので。
+        if(count >= 8)
+        {
+            winText.text = "Win";
+        }
     }
 }
